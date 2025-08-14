db323bc803cbb901ff7c7c33d47cca5a3649a9c1
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-08-14 10:32:29 -0300

    feat: Adiciona teste Junit em email

---
+++ 0x01/email/src/Pessoa.java
@@ -0,0 +1,19 @@
+public class Pessoa {
+
+    public static boolean emailValid(String email) {
+        if (email == null) {
+            return false;
+        }
+
+        if (!email.contains("@")) {
+            return false;
+        }
+
+        if (email.length() > 50) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
---
+++ 0x01/email/src/TesteEmail.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TesteEmail {
+
+    @Test
+    public void testar_email_com_arroba() {
+        String email = "email_teste@dominio.com.br";
+        assertTrue(Pessoa.emailValid(email));
+    }
+
+    @Test
+    public void testar_email_sem_arroba() {
+        String email = "email_testedominio.com.br";
+        assertFalse(Pessoa.emailValid(email));
+    }
+
+    @Test
+    public void testar_email_mais_50_caracteres() {
+        String email = "email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br";
+        assertEquals(false, Pessoa.emailValid(email));
+    }
+}
